32ea4502fc29eb0f963983bf07a7fe9bc83144c8
Shova <laxmitandukar0@gmail.com>
2021-11-21 15:59:49 +1300

    app bar updated

--- src/component/PublicEvent_Details.js
+++ src/component/PublicEvent_Details.js
@@ -48,15 +48,6 @@ const useStyles = makeStyles((theme) => ({
 	cardMedia: {
 		paddingTop: '56.25%', // 16:9
 	},
-	link: {
-		margin: theme.spacing(1, 1.5),
-	},
-	cardHeader: {
-		backgroundColor:
-			theme.palette.type === 'light'
-				? theme.palette.grey[200]
-				: theme.palette.grey[700],
-	},
 }));
 
 
